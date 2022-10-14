616f9515b314e26a4488e624fa09dcf1cf08230b
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-14 13:04:55 -0500

    displaying logo on login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
@@ -10,6 +11,7 @@ function LoginPage() {
 
   return (
     <Container sx={{}}>
+      <img src={'images/logo-cropped.jpg'} style={{width: 150, height: 100}}/>
       <Typography variant="h4">Waffle's Spot</Typography>
       <LoginForm />
       <center>
