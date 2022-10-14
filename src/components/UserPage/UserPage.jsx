525cf40332b985029fae35fbd17ace967ca6733e
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-14 14:33:14 -0500

    added background color to Nav and to body css in App.css, added Nav bottom border, commented out Nav from App.jsx in order to be able to be more selective about which components display Nav.jsx

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import Nav from '../Nav/Nav.jsx';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      {/* <p>Your ID is: {user.id}</p> */}
-      <LogOutButton className="btn" />
+    <div>
+      <Nav />
+      <div className="container">
+        <h2>Welcome, {user.username}!</h2>
+        {/* <p>Your ID is: {user.id}</p> */}
+        <LogOutButton className="btn" />
+      </div>
     </div>
   );
 }
