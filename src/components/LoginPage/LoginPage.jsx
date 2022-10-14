cd2f02f1af24f121b42000427ba89c2800280dd4
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-14 16:43:31 -0500

    matched RegisterPage display to LoginPage display

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -19,7 +19,7 @@ function LoginPage() {
         alignItems="center"
         justifyContent="center"
       >
-        <img src={'images/logo-cropped.jpg'} style={{width: 150, height: 100}}/>
+        <img src={'images/logo-cropped.jpg'} style={{width: 150, height: 100, border: 'solid'}}/>
         <br />
         <Typography variant="h4">Waffle's Spot</Typography>
         <br />
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -2,30 +2,39 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 import Container from '@mui/material/Container';
-import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 function RegisterPage() {
   const history = useHistory();
 
   return (
-    <Container>
-       <img src={'images/logo-cropped.jpg'} style={{width: 150, height: 100}}/>
-      <Typography variant="h4">Welcome to Waffle's Spot!</Typography>
-      <RegisterForm />
-
-      <center>
-        <Button
-          variant="contained"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </Button>
-      </center>
-    </Container>
+    <div style={{margin: 10, marginTop: 20}}>
+      <Grid 
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <img src={'images/logo-cropped.jpg'} style={{width: 150, height: 100, border: 'solid'}}/>
+        <br />
+        <Typography sx={{textAlign: 'center'}} variant="h4">Welcome to Waffle's Spot!</Typography>
+        <br />
+        <RegisterForm />
+        <center>
+          <Button
+            variant="contained"
+            onClick={() => {
+              history.push('/login');
+            }}
+          >
+            Login
+          </Button>
+        </center>
+      </Grid>
+    </div>
   );
 }
 
