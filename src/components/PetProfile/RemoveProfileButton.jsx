68ba195137962eacf02ad8b0a9bfdb4fe4d49ff0
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-17 11:46:30 -0500

    user is asked to confirm pet profile removal using MUI dialog in RemoveProfileButton.jsx

--- src/components/PetProfile/RemoveProfileButton.jsx
+++ src/components/PetProfile/RemoveProfileButton.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import {useDispatch} from 'react-redux';
 import {useHistory, useParams} from 'react-router-dom';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import './PetProfile.css';
 
 
@@ -26,42 +28,35 @@ const RemoveProfileButton = () => {
     }
 
     const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
-      
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClickClose = () => {
+        setOpen(false);
+    }
+
     return  <>
                 <Button 
-                    onClick={() => removeProfile(petid)} 
+                    onClick={handleClickOpen} 
                     className="remove-profile-btn" 
                     variant="contained" 
                     size="small">
                         REMOVE PROFILE
                     </Button>
-                    {/* <Modal
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        <Box sx={style}>
-                            <Typography variant="h6" component="h2">
-                                Are you sure you want to remove this pet's profile?
-                            </Typography>
-                            <Button>No, I don't.</Button>
-                            <Button>Yes, I'm sure.</Button>
-                        </Box>
-                    </Modal> */}
-
+                <Dialog
+                    open={open}
+                    onClose={handleClickClose}
+                >
+                    <DialogTitle>
+                        {"Remove this pet's profile?"}
+                    </DialogTitle>
+                    <DialogActions>
+                        <Button onClick={handleClickClose} variant="contained">No</Button>
+                        <Button onClick={() => removeProfile(petid)}>Yes</Button>
+                    </DialogActions>
+                </Dialog>
             </>
 }
 
