cbeecb4ac4d51feccdef33a57995abad63675904
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-18 19:15:06 -0500

    updated /home  Route in App.jsx so that if the user is not logged in they are taken to the LoginPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -130,7 +130,7 @@ function App() {
               <Redirect to="/user" />
               :
               // Otherwise, show the Landing page
-              <LandingPage />
+              <LoginPage />
             }
           </Route>
 
