95508012239656e2d39412388ee2efb0aade6159
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-20 17:47:08 -0500

    added dialog feature to ReminderItem so that when user clicks a reminder, a dialog box appears with the details, if any, regarding that care item

--- src/components/CareReminders/ReminderItem.jsx
+++ src/components/CareReminders/ReminderItem.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const ReminderItem = ({reminder}) => {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClickClose = () => {
+        setOpen(false);
+    }
+
     return <>   
-                <Grid container spacing={2} direction="row" display="flex" justifyContent="center">
+                <Grid container spacing={2} direction="row" display="flex" justifyContent="center" onClick={handleClickOpen}>
                     <Grid item sx={{display:"flex", alignItems:"center", justifyContent:"center"}}>
                         <Typography>{reminder.description}</Typography>
                     </Grid>
@@ -12,6 +29,17 @@ const ReminderItem = ({reminder}) => {
                         <CheckCircleOutlineIcon  fontSize="large"/>
                     </Grid>
                 </Grid>
+                <Dialog
+                    open={open}
+                    onClose={handleClickClose}
+                >
+                    <DialogTitle>
+                        {`Details for ${reminder.description}`}
+                    </DialogTitle>
+                    <DialogContent>
+                        {`${reminder.details}`}
+                    </DialogContent>
+                </Dialog>
             </>
 }
 
