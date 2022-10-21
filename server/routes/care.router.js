5ff25966b26d8c88f5cca1912710cf91342a077c
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-21 09:06:56 -0500

    updated care reminders get route to return all care items due today as well as the date_completed for each care item

--- server/routes/care.router.js
+++ server/routes/care.router.js
@@ -31,14 +31,13 @@ router.get('/reminders/:petid', (req, res) => {
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
     if(req.isAuthenticated()){
-        const remindersQueryText =  `SELECT * FROM "care_item" 
-                                    WHERE "pet_id" = $1 
-                                    AND "frequency" = 'daily' AND "start_date" <= NOW()
+        const remindersQueryText =  `SELECT "care_item".*, "pet_care_item"."date_complete" FROM "care_item"
+                                    JOIN "pet_care_item" ON "care_item"."id" = "pet_care_item"."care_item_id"
+                                    WHERE "pet_id" = $1 AND "frequency" = 'daily' AND "start_date" <= NOW()
                                     OR "pet_id" = $1 AND ("frequency" = 'weekly' AND to_char("start_date", 'D') = to_char(NOW(), 'D') AND "start_date" <= NOW())
                                     OR "pet_id" = $1 AND ("frequency" = 'monthly' AND to_char("start_date", 'DD') = to_char(NOW(), 'DD') AND "start_date" <= NOW())
                                     OR "pet_id" = $1 AND ("frequency" = 'yearly' AND to_char("start_date", 'DD MM') = to_char(NOW(), 'DD MM') AND "start_date" <= NOW())
-                                    ORDER BY "start_date"
-                                    LIMIT 5;`
+                                    ORDER BY "start_date";`
         pool.query(remindersQueryText, [req.params.petid])
             .then(result => {
             res.send(result.rows);
