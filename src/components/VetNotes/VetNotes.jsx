4e7ae1c5cf88867abc4943445f5949ad64269c72
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-22 11:34:07 -0500

    added component react imports to VetNotes

--- src/components/VetNotes/VetNotes.jsx
+++ src/components/VetNotes/VetNotes.jsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {useHistory, useParams} from 'react-router-dom';
+import Nav from '../Nav/Nav.jsx';
+
 const VetNotes = () => {
     return <>VetNotes</>
 }
