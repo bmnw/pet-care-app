8f5f6d8938239573cb371dc620310ffbd056ad79
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-22 12:46:15 -0500

    basic styling in VetNoteItem for mapped vet notes

--- src/components/VetNotes/VetNoteItem.jsx
+++ src/components/VetNotes/VetNoteItem.jsx
@@ -1,7 +1,23 @@
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
+const VetNoteItem = ({note}) => {
 
-const VetNoteItem = () => {
-    return <>Vet Note Item</>
+    return  <>
+                <Paper sx={{margin: 2, padding: 1}}>
+                    <Typography>Date: {new Date(note.date).toDateString()}</Typography>
+                    <Typography>Vet: {note.vet}</Typography>
+                    <br />
+                    <Box sx={{
+                        bgcolor: 'lightgray',
+                        p: 1
+                    }}>
+                        <Typography>{note.note}</Typography>
+                    </Box>
+
+                </Paper>
+            </>
 }
 
 export default VetNoteItem;
