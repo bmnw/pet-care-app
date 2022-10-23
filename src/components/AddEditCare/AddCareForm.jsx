9273feb5bc2daa7fe8d995f8cd53b2a320ada763
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-23 12:33:12 -0500

    AddCareForm updated with all Grid items and containers replaced with Box components, submitCareItem with updates

--- src/components/AddEditCare/AddCareForm.jsx
+++ src/components/AddEditCare/AddCareForm.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {useHistory, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import Textfield from '@mui/material/Textfield';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
@@ -75,62 +74,19 @@ const AddCareForm = ({showForm, setShowForm}) => {
                         </Select>
                     </FormControl>
                 </Box>
-                <Grid
-                    container
-                    spacing={2}
-                    direction="column"
-                    justifyContent="left"
-                >
-                    {/* <Grid item>
-                        <Typography>Description:</Typography>
-                    </Grid>
-                    <Grid item>
-                    <Textfield 
-                        sx={{backgroundColor: 'white'}}
-                        required
-                        value={careDescription}
-                        onChange={(event) => setCareDescription(event.target.value)}
-                        fullWidth 
-                    />
-                    </Grid> */}
-                    <Grid item>
-                        {/* <Typography>Frequency:</Typography> */}
-                    </Grid>
-                    <Grid
-                        container
-                        spacing={2}
-                        direction="row"
-                        justifyContent="center"
-                    >
-                        {/* Change these buttons to radio buttons */}
-                        {/* <Grid item>
-                            <Button onClick={() => setFrequency('daily')} sx={{width: 75}} variant="contained">DAILY</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button onClick={() => setFrequency('weekly')} sx={{width: 75}} variant="contained">WEEKLY</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button onClick={() => setFrequency('monthly')} sx={{width: 75}} variant="contained">MONTHLY</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button onClick={() => setFrequency('yearly')} sx={{width: 75}} variant="contained">YEARLY</Button>
-                        </Grid> */}
-                    </Grid>
-                    <Grid item>
-                        <Typography>Start Date (YYYY/MM/DD):</Typography>
-                    </Grid>
-                    <Grid item>
+                <br />
+                <Box>
+                    <Typography>Start Date (YYYY/MM/DD):</Typography>
                     <Textfield 
                         sx={{backgroundColor: 'white'}}
                         required
                         value={startDate}
                         onChange={(event) => setStartDate(event.target.value)}
                     />
-                    </Grid>
-                    <Grid item>
-                        <Typography>Care Details (optional):</Typography>
-                    </Grid>
-                    <Grid item>
+                </Box>
+                <br />
+                <Box>
+                    <Typography>Care Details (optional):</Typography>
                     <Textfield 
                         sx={{backgroundColor: 'white'}}
                         required
@@ -139,22 +95,12 @@ const AddCareForm = ({showForm, setShowForm}) => {
                         fullWidth 
                         multiline
                     />
-                    </Grid>
-                </Grid>
+                </Box>
                 <br />
-                <Grid
-                    container
-                    spacing={2}
-                    direction="row"
-                    justifyContent="center"
-                >
-                    <Grid item>
-                        <Button sx={{width: 100}} onClick={() => setShowForm(!showForm)} variant="contained">CANCEL</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button sx={{width: 100}} onClick={submitCareItem} variant="contained">SAVE</Button>
-                    </Grid>
-                </Grid>
+                <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                    <Button sx={{width: 100, marginRight: 1}} onClick={() => setShowForm(!showForm)} variant="contained">CANCEL</Button>
+                    <Button sx={{width: 100, marginLeft: 1}} onClick={submitCareItem} variant="contained">SAVE</Button>
+                </Box>
             </div>
 }
 
