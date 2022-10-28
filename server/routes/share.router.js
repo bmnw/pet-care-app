61022ef93ce91aa35cba0cbe6e2631fdf03cdc30
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-28 11:21:43 -0500

    created share router file and created route in server.js

---
+++ server/routes/share.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const { route } = require('./user.router');
+const router = express.Router();
+
+
+module.exports = router;
