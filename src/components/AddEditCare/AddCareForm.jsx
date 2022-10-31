03906941cb8c48c5acc83b4846745a1ed22835c8
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-10-31 09:59:35 -0500

    changes trying to setIcon to the clicked react icon onClick

--- src/components/AddEditCare/AddCareForm.jsx
+++ src/components/AddEditCare/AddCareForm.jsx
@@ -3,19 +3,9 @@ import { useDispatch } from 'react-redux';
 import {useParams} from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Textfield from '@mui/material/Textfield';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileDatePicker} from '@mui/x-date-pickers/MobileDatePicker';
 import moment from 'moment';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import InputAdornment from '@mui/material/InputAdornment';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import {FaTooth} from 'react-icons/fa';
 import {GiComb} from 'react-icons/gi';
@@ -30,7 +20,6 @@ import CareFrequencyFormItem from './CareFrequencyFormItem.jsx';
 import CareStartDateFormItem from './CareStartDateFormItem.jsx';
 import CareDetailsFormItem from './CareDetailsFormItem.jsx';
 
-
 const AddCareForm = ({showForm, setShowForm}) => {
 
     const dispatch = useDispatch();
@@ -40,7 +29,7 @@ const AddCareForm = ({showForm, setShowForm}) => {
     const [startDate, setStartDate] = useState('');
     const [frequency, setFrequency] = useState('');
     const [careDetails, setCareDetails] = useState('');
-    const [icon, setIcon] = useState('');
+    const [icon, setIcon] = useState('<FaTooth/>');
 
     const colorTheme = createTheme({
         palette: {
@@ -65,9 +54,9 @@ const AddCareForm = ({showForm, setShowForm}) => {
         setFrequency(event.target.value);
     }
 
-    const handleChangeIcon = (event) => {
-        console.log('in handleChangeIcon', event.target.value);
-        setIcon(event.target.value);
+    const handleChangeIcon = (iconInput) => {
+        console.log('in handleChangeIcon', iconInput);
+        setIcon(iconInput);
     }
 
     const handleDateChange = (newDate) => {
@@ -78,7 +67,6 @@ const AddCareForm = ({showForm, setShowForm}) => {
 
     const submitCareItem = () => {
         console.log('in submitCareItems', careDescription, frequency, startDate, careDetails);
-        // validate that careDescription, frequency, and stateDate have values
         if(careDescription && frequency && startDate){
             dispatch({type: 'SUBMIT_CARE_ITEM', payload: {
                     pet_id: petid, 
@@ -132,20 +120,20 @@ const AddCareForm = ({showForm, setShowForm}) => {
                 <ThemeProvider theme={colorTheme}>
                     <Box>
                         <Typography>Care Icon:</Typography>
+                        {icon}
                         <br />
                         <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                            <ButtonGroup variant="text">
-                                <Button sx={{fontSize: 30}} color="black"><FaTooth/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><GiComb/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><FaPoop/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><AiFillHeart/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><FaWalking/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><AiOutlineEye/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><GiMedicines/></Button>
-                                <Button sx={{fontSize: 30}} color="black"><FaBath/></Button>
+                            <ButtonGroup variant="text" onClick={(event) => handleChangeIcon(event.target.value)}>
+                                <Button sx={{fontSize: 30}} color="black" value="FaTooth"><FaTooth/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="GiComb"><GiComb/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="FaPoop"><FaPoop/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="AiFillHeart"><AiFillHeart/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="FaWalking"><FaWalking/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="AiOutlineEye"><AiOutlineEye/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="GiMedicines"><GiMedicines/></Button>
+                                <Button sx={{fontSize: 30}} color="black" value="FaBath"><FaBath/></Button>
                             </ButtonGroup>
                         </Box>
-
                     </Box>
                 </ThemeProvider>
 
