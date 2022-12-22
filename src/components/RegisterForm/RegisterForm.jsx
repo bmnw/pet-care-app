6a032c1849920471ac172066c7aa60aceeafc900
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-12-22 11:06:33 -0600

    refactored colorTheme code in RegisterForm and RegisterPage, updated RegisterForm title text to 'Register'

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,27 +4,12 @@ import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
-
-  const colorTheme = createTheme({
-    palette: {
-      orange: {
-        main: '#E27511',
-      },
-      white: {
-        main: '#F9F5F0',
-      },
-      blue: {
-        main: '#3D85C6',
-      }
-    }
-  });
 
   const registerUser = (event) => {
     event.preventDefault();
@@ -41,7 +26,7 @@ function RegisterForm() {
   return (
     <Container>
       <div className="formPanel">
-        <Typography variant="h5">Register User</Typography>
+        <Typography variant="h5">Register</Typography>
         {errors.registrationMessage && (
           <h3 className="alert" role="alert">
             {errors.registrationMessage}
@@ -73,9 +58,12 @@ function RegisterForm() {
         </div>
         <br />
         <div>
-          <ThemeProvider theme={colorTheme}>
-            <Button variant="contained" onClick={registerUser} color="orange">SIGN UP!</Button>
-          </ThemeProvider>
+            <Button 
+              variant="contained" 
+              onClick={registerUser} 
+              color="orange">
+                SIGN UP!
+              </Button>
         </div>
       </div>
     </Container>
